f958cd367a7d47a5d1a0c5527b9186604f83a546
Nathan Bryant <bryantnathanj@gmail.com>
2020-04-03 07:47:45 -0600

    build

---
+++ precache-manifest.9c5dcbd77c2a3f2ff573a44041308543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7b58373f9fb8545db122e69668b5f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c93d647a33b31232fa83",
+    "url": "./static/css/main.b7c79204.chunk.css"
+  },
+  {
+    "revision": "97c22844e6ca29603a2b",
+    "url": "./static/js/2.8c0f26ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8c0f26ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93d647a33b31232fa83",
+    "url": "./static/js/main.0a035f85.chunk.js"
+  },
+  {
+    "revision": "c097964322190b9636b0",
+    "url": "./static/js/runtime-main.52036a48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
